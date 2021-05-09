fae7693afb35e7b9557d2c7cd36e0798cca0a2ef
cryptum169 <ruoyangx@gmail.com>
2021-05-08 22:09:10 -0400

    Saving changes before merging in

--- evaluation/plot_traj.py
+++ evaluation/plot_traj.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-k = pd.read_csv("inertialOnlyTrajectory.txt")
+k = pd.read_csv("Trajectory.txt")
 
 fig = plt.figure(figsize = (16, 9))
 ax = plt.axes(projection ="3d")
